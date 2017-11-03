06a58836f32ee2677dbef572cf61c380e08747d5
song <sungyechun@icloud.com>
2017-11-03 16:37:05 +0800

    add text to textArea

--- src/MoveCursor/style.js
+++ src/MoveCursor/style.js
@@ -1,19 +1,3 @@
-// export const style ＝ {
-  // textButton: {
-  //   fontSize: '12px',
-  //   border: '1px solid',
-  //   height: '20px',
-  //   maxWidth: '50px',
-  //   overflow: 'hidden',
-  //   textOverflow: 'ellipsis',
-  //   whileSpace: 'nowrap',
-  //   lineHeight: '20px',
-  //   background: '#f6f6f6',
-  //   border: '1px solid #dbdada',
-  //   margin: '0 8px 8px 0',
-  //   padding: '0 5px'
-  // }
-// }
 export const MoveCursorStyle = {
   textButton: {
     fontSize: '12px',
